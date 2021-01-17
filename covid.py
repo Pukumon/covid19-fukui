f69c8ac85d5b2bfcd38172b6151b2226773393ca
Pukumon <yamada9374@gmail.com>
2021-01-17 14:26:08 +0900

    円グラフ文字サイズ変更

--- covid.py
+++ covid.py
@@ -64,7 +64,7 @@ st.write('年代別患者割合')
 df_age_pie = df['患者_年代'].value_counts().sort_index()
 
 fig = plt.figure(figsize=(10, 10))
-df_age_pie.plot(kind='pie', autopct=lambda p:'{:.1f}%'.format(p) if p>1 else '', startangle=90, counterclock=False , pctdistance=0.75, labeldistance=1.1)
+df_age_pie.plot(kind='pie', autopct=lambda p:'{:.1f}%'.format(p) if p>1 else '', startangle=90, counterclock=False , pctdistance=0.75, labeldistance=1.1, textprops = {"fontsize":"16"})
 plt.pie([100], colors='white', radius=0.5)
 plt.legend(df_age_pie.index, fancybox=True, fontsize=12, bbox_to_anchor=(1, 0.9))
 plt.title('年代別割合', y=0.46, fontsize=18, color='r')
@@ -121,10 +121,10 @@ if not (len(df_age_span.index) == 0):
 df_age_span_pie = df['患者_年代'][df_date1>span].value_counts().sort_index()
 if not (len(df_age_span.index) == 0): 
     fig = plt.figure(figsize=(10, 10))
-    df_age_span_pie.plot(kind='pie', autopct=lambda p:'{:.1f}%'.format(p) if p>1 else '', startangle=90, counterclock=False , pctdistance=0.75, labeldistance=1.1)
+    df_age_span_pie.plot(kind='pie', autopct=lambda p:'{:.1f}%'.format(p) if p>1 else '', startangle=90, counterclock=False , pctdistance=0.75, labeldistance=1.1, textprops = {"fontsize":"16"})
     plt.pie([100], colors='white', radius=0.5)
     plt.legend(df_age_span_pie.index, fancybox=True, fontsize=12, bbox_to_anchor=(1, 0.9))
-    pie_title = '年代別割合' + '直近' + str(number) + '日間'
+    pie_title = '年代別割合\n' + '直近' + str(number) + '日間'
     plt.title(pie_title,  y=0.46, fontsize=18, color='r')
     st.pyplot(fig)
 
